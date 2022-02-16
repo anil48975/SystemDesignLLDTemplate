54144d19b98d4c0828391c3228b74b2882f8d796
anil48975 <anil.tiwari.2007.5499@gmail.com>
2022-02-16 13:24:50 +0530

    Add files via upload

---
+++ systemdesignnew/aspects/TransactionAspect.java
@@ -0,0 +1,33 @@
+package systemdesignnew.aspects;
+
+import systemdesignnew.service.ConnectionService;
+
+import java.util.function.Function;
+
+public class TransactionAspect {
+    private final ConnectionService connectionService;
+    private TransactionAspect(ConnectionService connectionService){
+        this.connectionService = connectionService;
+    }
+    public static TransactionAspect getInstance(ConnectionService connectionService) {
+        return Holder.getInstance(connectionService);
+    }
+    public <T, R> Function<T, R> makeTransactional(Function<T, R> fun) {
+        return (t) -> {
+            try {
+                connectionService.starTx();
+                return fun.apply(t);
+            } finally {
+                connectionService.commitTx();
+            }
+        };
+    }
+
+    private static class Holder {
+        private static TransactionAspect transactionAspect;
+        private static TransactionAspect getInstance(ConnectionService connectionService) {
+            transactionAspect = new TransactionAspect(connectionService);
+            return transactionAspect;
+        }
+    }
+}
