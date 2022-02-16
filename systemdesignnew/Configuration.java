951abdfd92d96220a55074b185da6de24b7711e7
anil48975 <anil.tiwari.2007.5499@gmail.com>
2022-02-16 13:23:30 +0530

    Add files via upload

---
+++ systemdesignnew/Configuration.java
@@ -0,0 +1,49 @@
+package systemdesignnew;
+
+import systemdesignnew.aspects.ExceptionAspect;
+import systemdesignnew.aspects.TransactionAspect;
+import systemdesignnew.repository.ItemRepository;
+import systemdesignnew.service.ConnectionService;
+import systemdesignnew.service.ItemService;
+import systemdesignnew.service.ItemServiceImpl;
+import systemdesignnew.serviceproviders.ExecutorServiceProvider;
+import systemdesignnew.serviceproviders.ExecutorServiceProviderImpl;
+
+import java.util.function.Supplier;
+
+public class Configuration {
+    private Configuration() {
+        System.out.println("Configuration constructor should be called only once as it is a singleton service");
+    }
+    public static Configuration getInstance() {
+        return ConfigurationHolder.configuration;
+    }
+
+    ItemService itemService() {
+        return new ItemServiceImpl(itemRepository().get(), exceptionAspect(),
+                transactionAspect(), executorServiceProvider());
+    }
+
+    Supplier<ItemRepository> itemRepository() {
+        return () -> ItemRepository.getInstance();
+    }
+
+    ExecutorServiceProvider executorServiceProvider() {
+        return ExecutorServiceProviderImpl.INSTANCE;
+    }
+
+    TransactionAspect transactionAspect() {
+        return TransactionAspect.getInstance(connectionService());
+    }
+    ConnectionService connectionService() {
+        return new ConnectionService();
+    }
+
+    ExceptionAspect exceptionAspect() {
+        return new ExceptionAspect();
+    }
+
+    private static class ConfigurationHolder {
+        private static Configuration configuration = new Configuration();
+    }
+}
