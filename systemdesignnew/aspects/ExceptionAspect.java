22f2014810c9c9e6afe669dd258dabbe2fe503a4
anil48975 <anil.tiwari.2007.5499@gmail.com>
2022-09-09 14:55:15 +0530

    Update ExceptionAspect.java

--- systemdesignnew/aspects/ExceptionAspect.java
+++ systemdesignnew/aspects/ExceptionAspect.java
@@ -3,14 +3,14 @@ package systemdesignnew.aspects;
 import java.util.function.Function;
 
 public class ExceptionAspect {
-    public <T, R> Function<T, R> handleException(Function<T, R> fun) {
+   public <T, R> Function<T, R> handleException(Function<T, R> fun) {
         System.out.println("Exception handled.");
-        try {
-            return (t) -> {
+        return (t) -> {
+            try {
                 return fun.apply(t);
-            };
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        };
     }
 }
